bd1323c37212225a04e8e6b356007106625510c1
Claudiano Lima <claudianopereira047@gmail.com>
2021-11-23 20:46:12 -0300

    Configuração theme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 const Home: React.FC = () => {
   return (
     <>
-      <h1>Configuração global nextJS</h1>
+      <h1>sdadas</h1>
     </>
   );
 };
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,5 +1,20 @@
 const theme = {
-  colors: {},
+  colors: {
+    background: '#faf8f5',
+    white: '#ffffff',
+    aquaMarine: '#53ddbd',
+    greyishBrown: '#3e3e3e',
+    greyish: '#a9a8a8',
+    pinkishGrey: '#cfcfcf',
+    gunmetal: '#52575d',
+    steelGrey: '#7d8185',
+    paleGrey: '#e3e9ef',
+    darkGreenBlue: '#1a6050',
+    black: '#272727',
+    blackTwo: '#000000',
+    blackThree: '#1c1c1c',
+    greyishBrownRGBA: '',
+  },
 };
 
 export default theme;
